46ffca15d1df7f97d8f3eb1ace711f912584531d
Kyla Jang <kylajang@csu.fullerton.edu>
2021-02-03 23:30:39 -0800

    Do first half of Lab Part 6

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*                                                                             
+Name: Kyla Jang                                                                
+CWID: 888078243                                                                
+Email: kylajang@csu.fullerton.edu                                              
+*/
+
+int main()
+{
+  int two;
+  int three;
+  int addition_result;
+  int division_result;
+  double omg_pi = 3.14;
+  double pi_multiply = 2.1;
+  double mupltiply_result;
+
+  two = 2;
+  three = 3;
+  addition_result = two + three;
+  division_result = 3 / 2;
+  multiply_result = omg_pi * pi_multiply;
+
+  return 0;
+}
+
