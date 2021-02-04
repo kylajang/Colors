866529a2b9a551140d5e9b9c2cc2c25e705f2215
Kyla Jang <kylajang@csu.fullerton.edu>
2021-02-04 00:20:57 -0800

    Do second half of Lab Part 6

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ CWID: 888078243
 Email: kylajang@csu.fullerton.edu                                              
 */
 
+#include <iostream>
+	using std::cout;
+	using std::endl;
+
 int main()
 {
   int two;
@@ -12,7 +16,7 @@ int main()
   int division_result;
   double omg_pi = 3.14;
   double pi_multiply = 2.1;
-  double mupltiply_result;
+  double multiply_result;
 
   two = 2;
   three = 3;
@@ -20,6 +24,9 @@ int main()
   division_result = 3 / 2;
   multiply_result = omg_pi * pi_multiply;
 
+  cout << "Result of integer addition: " << addition_result << endl;
+  cout << "Result of double multiplication: " << multiply_result << endl;
+  cout << "Result of integer division: " << division_result << endl;
+
   return 0;
 }
-
